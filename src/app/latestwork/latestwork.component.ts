e30ff3439872059bdb89f97d2a9a9cc8b4acb6d6
Michelle <michelle-heintze@web.de>
2022-01-12 07:18:31 +0100

    test

---
+++ src/app/latestwork/latestwork.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-latestwork',
+  templateUrl: './latestwork.component.html',
+  styleUrls: ['./latestwork.component.scss']
+})
+export class LatestworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
